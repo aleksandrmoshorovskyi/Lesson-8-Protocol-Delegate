59475ac24b36278b6ee8732a1e90b6430aebc976
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-03-19 15:59:35 +0200

    Initial Commit

---
+++ Lesson-8-Protocol-Delegate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson-8-Protocol-Delegate
+//
+//  Created by Aleksandr Moroshovskyi on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
